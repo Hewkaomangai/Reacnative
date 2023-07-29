5a92da5c1843dd2a96c2fa67da7687dc6e2ee260
Hewkaomangai <bu.chanisorn_st@tni.ac.th>
2023-07-29 18:20:17 +0700

    Recap TextInput

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
-import DisplayandImage from './components/DisplayandImage';
-import LotsOfGreeting from './components/LotsOfGreeting';
-import CustomText from './components/CustomText';
+// import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
+// import DisplayandImage from './components/DisplayandImage';
+//import LotsOfGreeting from './components/LotsOfGreeting';
+//import CustomText from './components/CustomText';
+// import Counter from './components/Counter';
+// import MyInput from './components/MyInput';
+// import IncrementCounter from './components/IncrementCounter';
+// import Form from './components/Form';
+// import Login from './components/Login';
+import RecapLogin from './components/RecapLogin';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <ViewBoxesWithColorAndText/> */}
+    <View style = {styles.container}>
+      {/* <ViewBoxesWithColorAndText /> */}
       {/* <DisplayandImage/> */}
       {/* <LotsOfGreeting/> */}
-      <CustomText/>
+      {/* <CustomText/> */}
+      {/* <Counter /> */}
+      {/* <MyInput/> */}
+      {/* <IncrementCounter/> */}
+      {/* <Form/> */}
+      {/* <Login/> */}
+      <RecapLogin/>
     </View>
   );
+
 }
 
-
-
 const styles = StyleSheet.create({
-  container:{
-     flex:1,  
-     justifyContent:'center',    
-     alignItems:'center'
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
- })
+});
